45e225f36df2ff14d185dd477480378724009f3a
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-15 18:58:18 +0100

    done by id

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const hamburgerPopup = document.getElementsByClassName('div.hamburger-menu');
-console.log(hamburgerPopup);
+const iconBars = document.getElementById('icon-bars');
+const iconClose = document.getElementById('icon-close');
+
+iconBars.addEventListener('click',
+    function() {
+        const hamburgerPopup = document.getElementById('popup');
+        console.log(hamburgerPopup);
+        hamburgerPopup.classList.add('active');
+    }
+);
+
+iconClose.addEventListener('click',
+    function() {
+        const hamburgerPopup = document.getElementById('popup');
+        console.log(hamburgerPopup);
+        hamburgerPopup.classList.remove('active');
+    }
+);
