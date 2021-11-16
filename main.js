95d41a1b33462d38d319cfb7743e89d36b1c5e69
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-16 09:48:20 +0100

    By class

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const iconBars = document.getElementById('icon-bars');
-const iconClose = document.getElementById('icon-close');
+// By ID:
+
+// const iconBars = document.getElementById('icon-bars');
+// const iconClose = document.getElementById('icon-close');
+
+// iconBars.addEventListener('click',
+//     function() {
+//         const hamburgerPopup = document.getElementById('popup');
+//         console.log(hamburgerPopup);
+//         hamburgerPopup.classList.add('active');
+//     }
+// );
+
+// iconClose.addEventListener('click',
+//     function() {
+//         const hamburgerPopup = document.getElementById('popup');
+//         console.log(hamburgerPopup);
+//         hamburgerPopup.classList.remove('active');
+//     }
+// );
+
+// By Class, not editing html:
+
+const iconClose = document.querySelector('.close');
+const iconBars = document.querySelector('.header-right > a');
+const hamburgerPopup = document.querySelector('.hamburger-menu');
+console.log(iconBars);
+console.log(iconClose);
+
 
 iconBars.addEventListener('click',
     function() {
-        const hamburgerPopup = document.getElementById('popup');
         console.log(hamburgerPopup);
         hamburgerPopup.classList.add('active');
     }
@@ -11,7 +37,7 @@ iconBars.addEventListener('click',
 
 iconClose.addEventListener('click',
     function() {
-        const hamburgerPopup = document.getElementById('popup');
+        const hamburgerPopup = document.querySelector('.hamburger-menu');
         console.log(hamburgerPopup);
         hamburgerPopup.classList.remove('active');
     }
